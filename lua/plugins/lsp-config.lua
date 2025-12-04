0904e7579377c2d03d6dce17b0e78966bf7c8803
Jay Marzan <jaymarzan36@gmail.com>
2025-12-04 11:27:30 -0700

    Snippets and autocomplete

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -5,9 +5,13 @@ return {
          require("mason").setup()
       end,
    },
+
    {
       "williamboman/mason-lspconfig.nvim",
+      dependencies = { "neovim/nvim-lspconfig" },
       config = function()
+         local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
          require("mason-lspconfig").setup({
             ensure_installed = {
                "lua_ls",
@@ -18,32 +22,34 @@ return {
                "cssls",
                "clangd",
             },
-         })
-      end,
-   },
-   {
-      "neovim/nvim-lspconfig",
-      config = function()
-         local lsps = {
-            { "lua_ls" },
-            { "pyright" },
-            { "jdtls" },
-            { "ts_ls" },
-            { "html" },
-            { "cssls" },
-            { "clangd" },
-         }
-         vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover, {})
-         vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, {})
-         vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 
-         for _, lsp in pairs(lsps) do
-            local name, config = lsp[1], lsp[2]
-            vim.lsp.enable(name)
-            if config then
-               vim.lsp.config(name, config)
-            end
-         end
+            handlers = {
+               -- Default handler for all servers
+               function(server)
+                  require("lspconfig")[server].setup({
+                     capabilities = capabilities,
+                  })
+               end,
+
+               -- Example: custom config for lua_ls
+               ["lua_ls"] = function()
+                  require("lspconfig").lua_ls.setup({
+                     capabilities = capabilities,
+                     settings = {
+                        Lua = {
+                           diagnostics = { globals = { "vim" } },
+                           workspace = { checkThirdParty = false },
+                        },
+                     },
+                  })
+               end,
+            },
+         })
+
+         vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover)
+         vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition)
+         vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
       end,
    },
 }
+
